40b4707971501e562d0824aaa470bb29228278c8
RifatTauwab <mdruts@gmail.com>
2016-11-04 00:47:52 +0600

    Add files via upload
    swap character upper case to lower case and vice versa from string 
    example : HelLo
    output : hELlO

---
+++ swap_char.py
@@ -0,0 +1,11 @@
+def swap_char(sentance):
+    sentance = list(sentance)
+    for i in range(len(sentance)):    
+        if sentance[i].isalpha():
+            if ord(sentance[i])<91:        
+                sentance[i] = chr(ord(sentance[i])+32)
+            else:          
+                sentance[i] =  chr(ord(sentance[i])-32)
+    return ''.join(sentance)
+
+print swap_char("Tauwab")
